657ee92be28b4ab7a87d3a2b5962bedbce3ed11b
Nurali <nurali27@proton.me>
2024-10-18 20:36:44 +0500

    🎉 feat(server): Add doctors
    Changelog: feature

--- backend/app/controllers/views.py
+++ backend/app/controllers/views.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Body, HTTPException
 from fastapi.responses import JSONResponse
 from typing import Dict
 from app.models.schemas import User
-from app.services.user import add_user,get_user_by_tg_id
+from app.models.schemas import Doctors
+from app.services.user import add_user, get_user_by_tg_id
 from database.settings import doc_orders
 from openai import OpenAI
 import hashlib, os, hmac
 import asyncio
 
-API_KEY=os.getenv("OPENAI_API_KEY") 
+API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=API_KEY)
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -17,21 +18,14 @@ if not TELEGRAM_BOT_TOKEN:
 
 SECRET_KEY = hashlib.sha256(TELEGRAM_BOT_TOKEN.encode()).digest()
 
-
 async def request_to_openai(xd):
     completion = client.chat.completions.create(
-        model = "gpt-4o-mini",
+        model="gpt-4o-mini",
         messages=[
-        {"role": "system", "content": "Ты консультант и помощник ИИ в сфере медицины.Отвечай подробно и лаконично пациентам по их запросу."},
-
-        {
-            "role": "user",
-            "content": f"{xd}"
-        }
-    ]
-
+            {"role": "system", "content": "Ты консультант и помощник ИИ в сфере медицины.Отвечай подробно и лаконично пациентам по их запросу."},
+            {"role": "user", "content": f"{xd}"}
+        ]
     )
-
     return completion.choices[0].message
 
 async def check_init_data(init_data: dict, secret_key: bytes) -> bool:
@@ -39,9 +33,7 @@ async def check_init_data(init_data: dict, secret_key: bytes) -> bool:
     secret_hash = hmac.new(secret_key, check_string.encode(), hashlib.sha256).hexdigest()
     return secret_hash == init_data.get("hash")
 
-
 api_router: APIRouter = APIRouter()
-
 
 @api_router.post("/auth", response_model=User)
 async def api_auth(body: Dict[str, str] = Body(...)):
@@ -50,38 +42,70 @@ async def api_auth(body: Dict[str, str] = Body(...)):
     name: str = body.get("name")
     surname: str = body.get("surname")
     avatar_url: str = body.get("avatar_url")
-    
-    # if not await check_init_data(body, SECRET_KEY):
-        # raise HTTPException(status_code=403, detail="Invalid initData")
 
     if not tg_id:
         return JSONResponse(content={"result": 400, "data": "Неверный формат данных."}, status_code=400)
 
     try:
-        existing_user = await get_user_by_tg_id(tg_id=tg_id)  
+        existing_user = await get_user_by_tg_id(tg_id=tg_id)
         if existing_user:
             return JSONResponse(content={"result": 400, "data": "Пользователь с таким email уже существует."}, status_code=400)
-        
+
         user = await add_user(
-            tg_id=tg_id,name=name,surname=surname,
-            avatar_url=avatar_url,username=username
-                )
+            tg_id=tg_id, name=name, surname=surname,
+            avatar_url=avatar_url, username=username
+        )
         data = await request_to_openai(xd=name)
         print(data)
         return JSONResponse(content={"result": 201, "user": user}, status_code=201)
-    
-    except Exception as e:  
+
+    except Exception as e:
         print(e)
         return JSONResponse(content={"result": 500, "data": "Произошла ошибка."}, status_code=500)
-
 
 @api_router.get("/auth")
 async def api_auth(tg_id: int):
     user = await doc_orders.find_one({"tg_id": tg_id})
-    
+
     if user is None:
         raise HTTPException(status_code=404, detail="Пользователь с таким telegram ID не найден.")
-    
 
     user["_id"] = str(user["_id"])
     return {"user": user}
+
+@api_router.get("/doctors/{speciality}")
+async def api_get_doctor(speciality: str):
+    doctors = await doc_orders.find().to_list({"speciality": speciality})
+
+    if not doctors:
+        raise HTTPException(status_code=404, detail="Доктора с такой специальностей не найдены.")
+
+    return {"doctors": doctors}
+
+@api_router.post("/doctors" ,response_model=Doctors)
+async def api_add_doctor(body: Dict[str, str] = Body(...)):
+    doctor_id: int = body.get("doctor_id")
+    name: str = body.get("name")
+    surname: str = body.get("surname")
+    avatar_url: str = body.get("avatar_url")
+    speciality: str = body.get("speciality")
+    experience: int = body.get("experience")
+    rating: float = body.get("rating")
+    price: float = body.get("price")
+    description: str = body.get("description")
+    email: str = body.get("email")
+
+    if not doctor_id:
+        return JSONResponse(content={"result": 400, "data": "Неверный формат данных."}, status_code=400)
+
+    try:
+        doctor = await add_doctor(
+            doctor_id=doctor_id, name=name, surname=surname,
+            avatar_url=avatar_url, speciality=speciality, experience=experience,
+            rating=rating, price=price, description=description, email=email
+        )
+        return JSONResponse(content={"result": 201, "doctor": doctor}, status_code=201)
+
+    except Exception as e:
+        print(e)
+        return JSONResponse(content={"result": 500, "data": "Произошла ошибка."}, status_code=500)
--- backend/server/init.py
+++ backend/server/init.py
@@ -1,35 +1,29 @@
 from fastapi import FastAPI
-from database.settings import  database, check_connection
-
+from database.settings import database, check_connection
 import yaml
-
 from fastapi.middleware.cors import CORSMiddleware
 
 
 async def create_app() -> FastAPI:
     app = FastAPI(docs_url="/")
-        
+
     origins = [
         "http://localhost:5002",
-        
     ]
 
-    
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  
+        allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=["*"],  
-        allow_headers=["*"],  
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     try:
         await check_connection()
-        
+
         if "decentrathon" not in await database.list_collection_names():
             print("Collection 'carts' does not exist. Creating...")
-            
-            
             print("Collection 'carts' created.")
         else:
             print("Collection 'carts' already exists.")
@@ -42,14 +36,12 @@ async def create_app() -> FastAPI:
     def custom_openapi():
         if app.openapi_schema:
             return app.openapi_schema
-        app.openapi_schema = app.get_openapi()  
-    
-        
-        
-        
-    # app.openapi = custom_openapi
-    
+        app.openapi_schema = app.get_openapi()
+
+        # app.openapi = custom_openapi
+
     return app
+
 
 def register_views(app: FastAPI):
     from app.controllers.views import api_router
