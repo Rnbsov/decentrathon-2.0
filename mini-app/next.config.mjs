cf14639cb5ba06a406a25edee366d599982898ec
Rnbsov <rnbsov@gmail.com>
2024-10-18 14:45:51 +0500

    🍇 build(app): Some modification to `NextJs` build and script
    Changelog: build

--- mini-app/next.config.mjs
+++ mini-app/next.config.mjs
@@ -3,6 +3,8 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/core/i18n/i18n.ts')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true,
+}
 
 export default withNextIntl(nextConfig)
