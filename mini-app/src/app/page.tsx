b078629fdf68db9dc1dad922fab5b3c080ff3b8d
Rnbsov <rnbsov@gmail.com>
2024-10-18 20:15:50 +0500

    ✨ ui(app): Add user greeting message
    Changelog: ui

--- mini-app/src/app/page.tsx
+++ mini-app/src/app/page.tsx
@@ -1,51 +1,27 @@
 'use client'
 
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui'
 import { useTranslations } from 'next-intl'
 
-import { Link } from '@/components/Link/Link'
-import { LocaleSwitcher } from '@/components/LocaleSwitcher/LocaleSwitcher'
 import { Page } from '@/components/Page'
 
-import tonSvg from './_assets/ton.svg'
+import { useMiniAppStore } from '@/core/store/MiniAppStore'
 
 export default function Home() {
   const t = useTranslations('i18n')
 
+  const initData = useMiniAppStore((state) => state.initData)
+
   return (
     <Page back={false}>
-      <List>
-        <Section
-          header='Features'
-          footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
-        >
-          <Link href='/ton-connect'>
-            <Cell
-              before={<Image src={tonSvg.src} style={{ backgroundColor: '#007AFF' }} />}
-              subtitle='Connect your TON wallet'
-            >
-              TON Connect
-            </Cell>
-          </Link>
-        </Section>
-        <Section
-          header='Application Launch Data'
-          footer='These pages help developer to learn more about current launch information'
-        >
-          <Link href='/init-data'>
-            <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
-          </Link>
-          <Link href='/launch-params'>
-            <Cell subtitle='Platform identifier, Mini Apps version, etc.'>Launch Parameters</Cell>
-          </Link>
-          <Link href='/theme-params'>
-            <Cell subtitle='Telegram application palette information'>Theme Parameters</Cell>
-          </Link>
-        </Section>
-        <Section header={t('header')} footer={t('footer')}>
-          <LocaleSwitcher />
-        </Section>
-      </List>
+      <div className='text-black p-3'>
+        <p className='font-nunito'>
+          {t('greeting')},<br />
+          <span className='font-bold'>
+            {(initData?.user?.firstName ?? '') + ' ' + (initData?.user?.lastName ?? '')}
+          </span>{' '}
+          👋
+        </p>
+      </div>
     </Page>
   )
 }
