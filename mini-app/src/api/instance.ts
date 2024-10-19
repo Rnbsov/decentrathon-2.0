89b64cc389a8281c2396a4cf757e3062bc65c5bc
Rnbsov <rnbsov@gmail.com>
2024-10-20 02:00:22 +0500

    👾 fix(app): Update default api url
    Changelog: fix

--- mini-app/src/api/instance.ts
+++ mini-app/src/api/instance.ts
@@ -1,7 +1,7 @@
 import xior, { XiorRequestConfig } from 'xior'
 
 const options: XiorRequestConfig = {
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5002/',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5002/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
