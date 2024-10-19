0a961156621a6fcf79f483d10b99bcc0001e7664
Rnbsov <rnbsov@gmail.com>
2024-10-19 11:19:55 +0500

    ✨ ui(app): Add need help section
    Changelog: ui

--- mini-app/src/app/(main)/profile/page.tsx
+++ mini-app/src/app/(main)/profile/page.tsx
@@ -1,19 +1,20 @@
 'use client'
 
-import { profileLinks } from '@/core/constants'
-import { useMiniAppStore } from '@/core/store/MiniAppStore'
 import { Avatar, Headline, List, Subheadline } from '@telegram-apps/telegram-ui'
 import { Edit } from 'lucide-react'
 import Link from 'next/link'
+
+import styles from './styles.module.css'
+import { profileLinks } from '@/core/constants'
+import { useMiniAppStore } from '@/core/store/MiniAppStore'
+import { cn } from '@/core/utils'
 
 function Profile() {
   const initData = useMiniAppStore((state) => state.initData)
 
   return (
     <List className='w-full min-h-screen text-black'>
-      <h2  className='h2-bold flex-center'>
-        Profile
-      </h2>
+      <h2 className='h2-bold flex-center'>Profile</h2>
       <div className='relative'>
         <div className='relative bg-primary/50  w-full h-32 rounded-xl'>
           <button className='bg-black/50 rounded-full p-2 absolute right-1 top-1'>
@@ -21,7 +22,10 @@ function Profile() {
           </button>
         </div>
         <div className='absolute left-1/2 top-[95%] transform -translate-x-1/2 -translate-y-1/2 flex-center flex-col'>
-          <Avatar size={128} src={initData?.user?.photoUrl || 'https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649'} />
+          <Avatar
+            size={128}
+            src={initData?.user?.photoUrl || 'https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649'}
+          />
           <Headline weight='1'>{(initData?.user?.firstName ?? '') + ' ' + (initData?.user?.lastName ?? '')}</Headline>
           <Subheadline className='text-slate-500'>@{initData?.user?.username}</Subheadline>
         </div>
@@ -37,6 +41,16 @@ function Profile() {
               </Link>
             </button>
           ))}
+        </div>
+
+        <div className='bg-primaryPurple rounded-2xl min-h-32 my-3 p-4 mx-2'>
+          <p className={cn(styles.needhelp, 'font-bold p-4 text-lg')}>Need help?</p>
+
+          <p className='p-4'>
+            <p className='text-black/50'> Feel free to write to an email:</p>
+            <br />
+            <span className='font-bold'>help@med.kz</span>
+          </p>
         </div>
       </div>
     </List>
