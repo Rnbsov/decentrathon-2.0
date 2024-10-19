0d1e447f9fd58feefef07cce2d1188c8d9b7cfa8
Rnbsov <rnbsov@gmail.com>
2024-10-19 14:52:23 +0500

    ✨ ui(app): Categories and blob
    Changelog: ui

--- mini-app/src/app/pharmacy/page.tsx
+++ mini-app/src/app/pharmacy/page.tsx
@@ -1,5 +1,7 @@
-import { SearchBar } from '@/components/SearchBar/SearchBar'
+import { ChevronRight } from 'lucide-react'
 import Image from 'next/image'
+
+import { SearchBar } from '@/components/SearchBar/SearchBar'
 
 function Pharmacy() {
   return (
@@ -20,7 +22,7 @@ function Pharmacy() {
           </div>
         </div>
 
-        <div className='bg-[#C7F4C2] p-6 rounded-2xl shadow-md flex items-center justify-between mt-2 w-full'>
+        <div className='bg-[#C7F4C2] p-6 rounded-2xl font-nunito shadow-md flex items-center justify-between mt-2 w-full'>
           <div className='max-w-xs'>
             <div className='flex items-end'>
               <div className='flex'>
@@ -30,7 +32,7 @@ function Pharmacy() {
 
               {/* Offer text positioned at the bottom right */}
             </div>
-              <p className='right-0 bottom-0 text-sm text-gray-600 ml-14'>OFFER*</p>
+            <p className='right-0 bottom-0 text-sm text-gray-600 ml-14'>OFFER*</p>
             <p className='text-lg font-bold text-gray-800 mb-4'>On Health Products</p>
 
             <button className='bg-green-500 text-white font-bold py-2 px-6 rounded-full hover:bg-green-600 focus:outline-none mb-2'>
@@ -43,9 +45,45 @@ function Pharmacy() {
           </div>
 
           {/* Right Section: Medicine Images */}
-          <div className='flex items-end space-x-2'>
-            {/* Example Image placeholders */}
-            <Image src="/medecines.png" alt="Product 1" height={128} width={156} />
+          <div className='relative flex items-end space-x-2'>
+            {/* Green Circle/Blob */}
+            <div className='absolute bg-[#dff7d9] rounded-full size-28 -right-5 -top-8 z-0'></div>
+
+            <Image src='/medecines.png' alt='Product 1' height={128} width={156} className='relative z-10' />
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-2 mt-3 font-nunito'>
+          <div className='flex-between'>
+            <h2 className='text-[25px] font-bold leading-[140%] tracking-tighter text-black'>Popular Categories</h2>
+            <div className='flex'>
+              <button className='text-black'>See all</button>
+              <ChevronRight className='stroke-black' />
+            </div>
+          </div>
+
+          <div className='grid grid-cols-3 gap-4'>
+            {[
+              { title: 'Homeopathy', imageSrc: '/lemonlimeelectrolyte_png.png' },
+              { title: 'Ayurvedic', imageSrc: '/c2.png' },
+              { title: 'Vitamins & Supplement', imageSrc: '/c3.png' },
+              { title: 'Herbal', imageSrc: '/c4.png' },
+              { title: 'Supplements', imageSrc: '/c5.png' },
+              { title: 'Diabetes Care', imageSrc: '/c6.png' }
+            ].map((category) => (
+              <div className='flex flex-col gap-2 items-center' key={category.title}>
+                <div className='w-16 h-16 border-[#EFEDE9] border-[1.5px] rounded-xl p-2'>
+                  <Image
+                    src={category.imageSrc}
+                    alt={category.title}
+                    width={64}
+                    height={64}
+                    className='object-cover w-full h-full'
+                  />
+                </div>
+                <p className='text-black font-semibold text-center'>{category.title}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
