333505e9a8b762874aa3f23a114a10e5125447da
Rnbsov <rnbsov@gmail.com>
2024-10-19 09:48:59 +0500

    🎉 feat(app): Add Fab button redirecting to ai
    Changelog: feature

--- mini-app/src/app/page.tsx
+++ mini-app/src/app/page.tsx
@@ -1,8 +1,13 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import Link from 'next/link'
 
+import { CheckHealth } from '@/components/CheckHealth/CheckHealth'
 import { Page } from '@/components/Page'
+import { RecentCheckUps } from '@/components/RecentCheckUps/RecentCheckUps'
+import { Services } from '@/components/Services/Services'
 
 import { useMiniAppStore } from '@/core/store/MiniAppStore'
 
@@ -13,7 +18,7 @@ export default function Home() {
 
   return (
     <Page back={false}>
-      <div className='text-black p-3'>
+      <div className='text-black p-3 overflow-hidden'>
         <p className='font-nunito'>
           {t('greeting')},<br />
           <span className='font-bold'>
@@ -21,7 +26,19 @@ export default function Home() {
           </span>{' '}
           👋
         </p>
+
+        <Services />
+
+        <RecentCheckUps />
+
+        <CheckHealth />
       </div>
+
+      <Link href='/ai/home'>
+        <button className='fixed bottom-[7rem] right-6 bg-[#192521] font-bold size-[58px] flex-center rounded-2xl shadow-lg'>
+          <Image src='./fab.svg' alt='fab' width={48} height={48} />
+        </button>
+      </Link>
     </Page>
   )
 }
